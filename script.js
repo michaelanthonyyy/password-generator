e10d069e1ff07c359c2e144671d0eeba205773a2
Michael Medina <michaelanthony.medinaa@gmail.com>
2020-12-19 17:02:18 -0800

    added if statements for generated prompts and variables to initialize prompts

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-// declare arrays for each character set (multiple arrays or one nested array)
-// arrays should be made to include options for capital letters, lower case, numbers, and special characters
-// these arrays are for possible input choices for user when they go through following prompts
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 var specialCharacters = ["!",'"',"#","$","%","&","'",")","(","*","+",",","-",".","/",":",";","<",">","=","?","@","[","]","^","`","{","}","|","~"];
-
-// create prompts that set presidence for remainder of code. prompts should be length > lower > upper > numbers > special characters
-// this is going to be done using prompt functions. each character type has its own prompt
-// this is done in order to gather information to generate the result of each users choice 
-// ***At least one prompt must be chosen to proceed*** if not set alert to select at least one prompt
+var charactersConfirmed;
+var passwordLength;
+var userPassword;
 
 
-// using results of the prompt create password
-// functions and conditional statements are used to select between prompt answers and math.random is used to randomize password
-// this creates a password that is randomly generated between length and selected characters
+var passwordLength = prompt("How many characters would you like to use?")
+  if (passwordLength >= 8 && passwordLength <= 128) {
+  var upperCaseConfirm = confirm("Would you like to use Upper Case Letters?");
+  var lowerCaseConfirm = confirm("Would you like to use Lower Case Letters?");
+  var numbersConfirm = confirm("Would you like to use numbers?");
+  var specialCharactersConfirm = confirm("Would you like to use Special Characters?");  
+  }
 
+// Write password to the #password input
 
-//  display generated password as alert or print to page .. or both?
-// using prompt or window.print() 
-// done to give user their generated password based on their own selected criteria
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) 
+  
+
+function writePassword(){
+  var password = generatePassword();
+  var passwordText = document.querySelector("password");
+
+  passwordText.value = password;
+}
+
+  function generatePassword() {
+    var charactersConfirmed= ""
+    if (upperCaseConfirm === true) {
+      charactersConfirmed += upperCase;
+    }
+    if(lowerCaseConfirm === true) {
+      charactersConfirmed += lowerCase;
+    }
+    if(numbersConfirm === true) {
+      charactersConfirmed += numbers;
+    }
+    if(specialCharactersConfirm === true) {
+      charactersConfirmed += specialCharacters;
+    }
+
+
+    if 
+    for (i = passwordLength; i <= passwordLength; i++) {
+      charactersConfirmed.charAt(Math.floor(Math.random() * charactersConfirmed.length -1));
+    }     
+  }
+
+  console.log(password);
